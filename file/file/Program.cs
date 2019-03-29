b26d8db7473dcc0f84ad0e657891b10966c47964
divyadori <46336292+divyadori@users.noreply.github.com>
2019-03-29 02:56:57 -0500

    Add files via upload

---
+++ file/file/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+/*using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;*/
+using System.IO;
+namespace streamreaderexample
+{
+    public class streamreaderexample
+    {
+        public static void Main()
+        {
+            StreamReader reader = new StreamReader("test.txt");
+            using (reader)
+            {
+                int lineNumber = 0;
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    lineNumber++;
+                    Console.WriteLine("Line {0}: {1}", lineNumber, line);
+                    line = reader.ReadLine();
+                }
+                String[] array1 = Directory.GetFiles(@"C:\Users\Dell\source\repos");
+                Console.WriteLine("files in the directory");
+                foreach(string name in array1)
+                {
+                    Console.WriteLine(name);
+                }
+
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
